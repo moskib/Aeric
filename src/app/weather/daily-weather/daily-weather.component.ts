d163b51b558cc3e908872104a2f9f8d9a90761fe
moskib <moshekiba3@gmail.com>
2019-12-24 11:55:16 +0200

    updated project to use the observable

--- src/app/weather/daily-weather/daily-weather.component.ts
+++ src/app/weather/daily-weather/daily-weather.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LocationService } from 'app/services/location.service';
 import { BehaviorSubject } from 'rxjs';
 import { Location } from 'app/models/location.model';
+import { WeatherService } from 'app/services/weather.service';
 
 @Component({
   selector: 'aeric-daily-weather',
@@ -10,9 +11,16 @@ import { Location } from 'app/models/location.model';
 })
 export class DailyWeatherComponent implements OnInit {
 
-  constructor(private locationService: LocationService) { }
+  constructor(
+    private locationService: LocationService,
+    private weatherService: WeatherService
+  ) { }
 
   ngOnInit() {
+  }
+
+  get $currentWeather() {
+    return this.weatherService.fiveHourForecast;
   }
 
   get $currentLocation(): BehaviorSubject<Location> {
